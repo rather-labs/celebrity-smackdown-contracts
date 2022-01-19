c41582752deb8726a07743fcbf2fd239a402c4b4
scucchiero <scucchiero@gmail.com>
2022-01-18 22:17:27 -0300

    updated

--- polyjuice/contracts/DistributionWizard.sol
+++ polyjuice/contracts/DistributionWizard.sol
@@ -5,12 +5,15 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DistributionWizard is Ownable {
+    event Withdrawal(address to, uint256 amount, bytes32 key);
+
     IERC20 token;
     address treasury;
     uint256 public epoch;
     uint256 public initialTimestamp;
 
     mapping(address => uint256) public lastTimeWithdrawn;
+    mapping(address => bytes32) public lastKeyUsed;
 
     constructor(
         IERC20 _token,
@@ -27,6 +30,7 @@ contract DistributionWizard is Ownable {
         address _to,
         uint256 _amount,
         uint256 _expires,
+        bytes32 _key,
         bytes memory _signature
     ) public {
         bool isPermitValid = verify(
@@ -34,6 +38,7 @@ contract DistributionWizard is Ownable {
             _to,
             _amount,
             _expires,
+            _key,
             _signature
         );
         uint256 cantClaimUntil = lastTimeWithdrawn[_to] + epoch;
@@ -47,9 +52,15 @@ contract DistributionWizard is Ownable {
             block.timestamp > cantClaimUntil,
             "DistributionWizard: Can't withdraw yet"
         );
+        require(
+            lastKeyUsed[_to] != _key,
+            "DistributionWizard: Key already used"
+        );
 
         lastTimeWithdrawn[_to] = block.timestamp;
+        lastKeyUsed[_to] = _key;
         token.transferFrom(treasury, _to, _amount);
+        emit Withdrawal(_to, _amount, _key);
     }
 
     function verify(
@@ -57,9 +68,10 @@ contract DistributionWizard is Ownable {
         address _to,
         uint256 _amount,
         uint256 _expires,
+        bytes32 _key,
         bytes memory _signature
     ) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(_to, _amount, _expires);
+        bytes32 messageHash = getMessageHash(_to, _amount, _expires, _key);
         bytes32 ethSignedMessageHash = keccak256(
             abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
         );
@@ -70,9 +82,10 @@ contract DistributionWizard is Ownable {
     function getMessageHash(
         address _to,
         uint256 _amount,
-        uint256 _expires
+        uint256 _expires,
+        bytes32 _key
     ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_to, _amount, _expires));
+        return keccak256(abi.encodePacked(_to, _amount, _expires, _key));
     }
 
     function splitSignature(bytes memory sig)
