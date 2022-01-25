2f95b19b76f6a86298bacaba09a66ff3b2c87bab
eduardo.yuschuk <eduardo.yuschuk@gmail.com>
2022-01-25 12:39:43 -0800

    reading slammie

--- polyjuice/init.js
+++ polyjuice/init.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { ContractFactory } = require("ethers");
+const { ContractFactory, Contract } = require("ethers");
 const { PolyjuiceConfig } = require("@polyjuice-provider/base");
 const { PolyjuiceWallet, PolyjuiceJsonRpcProvider } = require("@polyjuice-provider/ethers");
 
@@ -9,7 +9,7 @@ const polyjuiceConfig = {
   //creatorId: 'polyjuice creator account id', // this is optional
   //defaultFromAddress: 'a default eth address, which will be used as a default from in ethereum transaction', // this is optional
   //abiItems: ['your abi items array'],
-  web3Url: process.env.WEB3_PROVIDER_URL, 
+  web3Url: process.env.WEB3_PROVIDER_URL,
 };
 
 const rpc = new PolyjuiceJsonRpcProvider(polyjuiceConfig, polyjuiceConfig.web3Url);
@@ -17,18 +17,19 @@ const deployer = new PolyjuiceWallet(process.env.DEPLOYER_PRIVATE_KEY, polyjuice
 //console.log(deployer);
 
 const slammie = require("./build/contracts/Slammie.json");
-console.log(slammie.abi);
+//console.log(slammie.abi);
 
-
-const contract = new ethers.Contract(
+(async () => {
+  const contract = new Contract(
     '0x89e271027eaC536c5d6EFC21310d69341bbA7B08',
-    'your contract abi',
-    signer
+    slammie.abi,
+    rpc//signer
   );
-  let overrides = {
-    gasLimit: 0x54d30,
-    gasPrice: 0x0,
-    value: 0x0,
-  };
-const txResponse = await contract.totalSupply(..args, overrides);
-console.log(txResponse);
+  // let overrides = {
+  //   gasLimit: 0x54d30,
+  //   gasPrice: 0x0,
+  //   value: 0x0,
+  // };
+  const txResponse = await contract.symbol();
+  console.log(txResponse);
+})();
